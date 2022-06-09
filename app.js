ba6e469da0118e1662603e4217e3f2dc4d5ecbad
pierovillata <ironpiero1@gmail.com>
2022-06-09 11:36:36 -0300

    cambios en getentrenos, agregada imaganes

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ console.log(redirectUri);
 
 // redireccion la app a la pagina de authorizacion de strava
 function getAuthRedirect() {
+    sessionStorage.clear();
     window.location.href = "https://www.strava.com/oauth/authorize?client_id=" + client_id + "&redirect_uri=" + redirectUri + "&response_type=code&scope=activity:read_all";
 
 }
@@ -58,25 +59,80 @@ function getTokenAccess() {
         });
 }
 
+function imagenSport(sport) {
+    var imagen = "";
+    if (sport == "Ride") {
+        imagen = "imagenes/deportes/1.png";
+    }
+    if (sport == "Run") {
+        imagen = "imagenes/deportes/2.png";
+    }
+    if (sport == "Workout") {
+        imagen = "imagenes/deportes/3.png";
+    }
+    if (sport == "Yoga") {
+        imagen = "imagenes/deportes/4.png";
+    }
+    return imagen;
+
+}
+
 function getEntrenos() {
+
+    $("#actividades").empty();
 
     var access_token = sessionStorage.getItem('access_token')
         // alert(access_token)
-    const url = "https://www.strava.com/api/v3/athlete/activities?access_token=" + access_token;
+    const url = "https://www.strava.com/api/v3/athlete/activities?page=1&per_page=18&access_token=" + access_token;
 
     fetch(url, {
         method: 'get',
-    }).then(function(res) {
-        var actividades = res.json();
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json()).then(function(res) {
 
-        var nroAct = actividades.length;
-        for (let i = 0; i < nroAct; i++) {
-            let entreno = JSON.parse(actividades[i]);
-            console.log(entreno);
-            let filaActividad = `<div><b>Nombre</b>` + entreno.name + `</div><br>`;
-            $("#actividades").append(filaActividad)
+        var entrenos = res;
+
+        if (entrenos == "" || entrenos == null) {
+            $.notify("Sin entrenos registrados", "info")
+        } else {
+
+            // $("#activividades").empty();
+
+            entrenos.forEach(entreno => {
+                var id = entreno.id;
+                var distancia = entreno.distance / 1000;
+                distancia = distancia.toFixed(2)
+                var tiempo = entreno.elapsed_time / 60;
+                tiempo = tiempo.toFixed(2)
+                var vel = entreno.average_speed * 3.6;
+                vel = vel.toFixed(2)
+                var tipo = entreno.type;
+                var img = imagenSport(tipo)
+                let filaActividad = `<div class="card card-sport alert-info  col-xs-12 col-sm-12 col-md-2 col-lg-2">
+                                            
+                                                <div class="alert-success card-header card-header-activity  ">
+                                                  <center> <b>` + tipo + `</b><img src="` + img + `" class="imgSport"/><br>
+                                                  <b>Lunes 01/01/2022</b><button class="btn btn-sm btn-info" onclick="getEntrenoId(` + id + `)">Ver</button>
+                                                  </center>                                                   
+                                                </div>
+                                                             
+                                               <center><div class="card-body">
+                                                <b>` + entreno.name + `</b><br>
+                                                <b>` + distancia + `</b>km<br>
+                                                <b>` + tiempo + `</b>min<br> 
+                                                <b>` + vel + `</b>km/h<br>                
+                                                </div></center>
+                
+                </div>`;
+                $("#actividades").append(filaActividad)
+            });
 
         }
+
+
     }).catch(function(error) {
 
         $.notify("Ha ocurrido un error al obtener entrenos de strava " + error, "error");
@@ -85,28 +141,41 @@ function getEntrenos() {
 
 }
 
-function mostrarActividades(res) {
+function getEntrenoId(Id) {
+    var access_token = sessionStorage.getItem('access_token')
+    var url = "https://www.strava.com/api/v3/activities/" + Id + "?access_token=" + access_token;;
 
-    console.log(actividades)
+    fetch(url, {
+        method: 'get',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
 
+    }).then(res => res.json()).then(function(resp) {
+
+        alert(resp);
+
+    }).catch(function(error) {
+        $.notify("Ha ocurrido un error al obtener el entreno" + error, "error")
+    })
 
 
 }
+
 
 function logout() {
 
     var token = sessionStorage.getItem('access_token')
 
-    let url = "https://www.strava.com/oauth/deauthorize"
+    let url = "https://www.strava.com/oauth/deauthorize?access_token=" + access_token;
     fetch(url, {
             method: 'post',
             headers: {
                 'Accept': 'application/json, text/plain, */*',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({
-                'access_token': token
-            })
+
 
         }).then(function(res) {
             alert("Aplicacion desautorizada" + sucess);
